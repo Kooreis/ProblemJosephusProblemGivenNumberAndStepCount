a2c8584d7cde03dbafa1631bf0b7db5c009247a2
Kooreis <kooreis.official@gmail.com>
2024-01-28 09:30:56 -0600

    Created the main class and method. Added user input for the number of people and step count. Printed the result of the josephus function.

--- solution.java
+++ solution.java
@@ -1,3 +1,9 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of people: ");
+        int n = scanner.nextInt();
+        System.out.println("Enter the step count: ");
+        int k = scanner.nextInt();
+        System.out.println("The chosen place is " + josephus(n, k));
+    }
