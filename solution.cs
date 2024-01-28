006f8b2e61ce77b5c2c927946e37ab472bef0f41
Kooreis <kooreis.official@gmail.com>
2024-01-28 09:30:56 -0600

    In this update, the user is prompted to enter the step count.

--- solution.cs
+++ solution.cs
@@ -7,5 +7,8 @@ class Program
     {
         Console.Write("Enter the number of people: ");
         int numberOfPeople = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the step count: ");
+        int stepCount = Convert.ToInt32(Console.ReadLine());
     }
 }
